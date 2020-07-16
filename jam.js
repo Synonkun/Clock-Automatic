964a2b76631bcb5290f2ce2194a4bca3cc4015e2
Synon <58498746+Synonkun@users.noreply.github.com>
2020-07-17 01:53:12 +0700

    Update jam.js

--- jam.js
+++ jam.js
@@ -3,7 +3,7 @@ function tick()
     var hours, minutes, seconds, ap, br, say, back;
     var intHours, intMinutes, intSeconds;
     var today;
-    var bg = document.getElementByTagName('body');
+    var bg = document.getElementById('body');
     today = new Date();
     
     intHours = today.getHours();
